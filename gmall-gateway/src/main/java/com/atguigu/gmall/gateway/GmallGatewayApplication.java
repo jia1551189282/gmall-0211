ddb79c40cfc63fa106431f038f8abb0e23948065
zjiajia <zjiajia@163.com>
2020-07-30 09:19:57 +0800

    网关搭建

---
+++ gmall-gateway/src/main/java/com/atguigu/gmall/gateway/GmallGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.gmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZJJ
+ * @date 2020/7/30 0030 上午 9:14
+ */
+
+@SpringBootApplication
+public class GmallGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GmallGatewayApplication.class,args);
+    }
+}
